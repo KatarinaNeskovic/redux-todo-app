3cf5cce17f0eaf4eb15b79c9e31151984a4cb5f1
Katarina Neskovic <katarina.neskovic@gmail.com>
2024-10-23 14:44:18 +0200

    edit mapping of todos

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -8,7 +8,7 @@ const TodoList=({ todos, clearAllTodos }) => {
   return (
     <div className="todo-list-container">
       <ul className="todo-list">
-        {todos && todos.map((todo) => (
+        {todos.map((todo) => (
           <TodoItem key={todo.id} todo={todo} /> // react always needs a unique key when rendering lists!
         ))}{" "}
       </ul>
