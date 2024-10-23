ac4fb6b29c87e0c9419316d1307c78faa8bde6b2
Katarina Neskovic <katarina.neskovic@gmail.com>
2024-10-23 14:38:24 +0200

    update TodoList import in App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import AddTodo from './components/AddTodo';
-import TodoList from './components/TodoList'
-import './App.css';
-import Filter from './components/Filter';
+import React from "react";
+import AddTodo from "./components/AddTodo";
+import { TodoList } from "./components/TodoList";
+import "./App.css";
+import Filter from "./components/Filter";
 
 function App() {
   return (
     <div className="App">
       <h1>Todo List</h1>
-      <div className='todo-container'> 
-      <AddTodo />
-      <Filter />
-      <TodoList />
-
-
+      <div className="todo-container">
+        <AddTodo />
+        <Filter />
+        <TodoList />
       </div>
     </div>
   );
